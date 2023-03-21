6e4557c0ee51cbae26155ad62598b70b220a9a81
Kamkoz <kamkoz93@gmail.com>
2023-03-21 09:46:56 +0100

    feat: improved refreshToken for different API err

--- src/app/services/refresh-token.interceptor.ts
+++ src/app/services/refresh-token.interceptor.ts
@@ -8,10 +8,60 @@ import {
 import { catchError, Observable, switchMap, throwError } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { STORAGE } from './storage';
+import { Router } from '@angular/router';
+import { ROUTES_DEF } from '../configuration/routes-definition';
+// @Injectable()
+// export class RefreshTokenInterceptor implements HttpInterceptor {
+//   constructor(
+//     private _authService: AuthService,
+//     private _router: Router,
+//     @Inject(STORAGE) private _storage: Storage
+//   ) {}
+//   intercept(
+//     request: HttpRequest<any>,
+//     next: HttpHandler
+//   ): Observable<HttpEvent<any>> {
+//     return next.handle(request).pipe(
+//       catchError((err) => {
+//         const refreshToken = this._storage.getItem('refreshToken');
+
+//         if (
+//           err.status === 401 &&
+//           err.error.message === 'Authentication required' &&
+//           refreshToken
+//         ) {
+//           return this._authService.refreshToken(refreshToken).pipe(
+//             switchMap((data) => {
+//               const newReq = request.clone({
+//                 headers: request.headers.set(
+//                   'Authorization',
+//                   `Bearer ${data.accessToken}`
+//                 ),
+//               });
+//               return next.handle(newReq);
+//             })
+//           );
+//         }
+//         // else {
+//         //   if (
+//         //     err.status === 403 &&
+//         //     err.error.message === 'Token is invalid' &&
+//         //     refreshToken
+//         //   ) {
+//         //     this._router.parseUrl(ROUTES_DEF.LOGIN);
+//         //   }
+//         // }
+//         return throwError(() => err);
+//       })
+//     );
+//   }
+// }
+
 @Injectable()
 export class RefreshTokenInterceptor implements HttpInterceptor {
   constructor(
     private _authService: AuthService,
+    private _router: Router,
     @Inject(STORAGE) private _storage: Storage
   ) {}
   intercept(
@@ -23,12 +73,9 @@ export class RefreshTokenInterceptor implements HttpInterceptor {
         const refreshToken = this._storage.getItem('refreshToken');
 
         if (
-          (err.status === 403 &&
-            err.error.message === 'Token is invalid' &&
-            refreshToken) ||
-          (err.status === 401 &&
-            err.error.message === 'Authentication required' &&
-            refreshToken)
+          err.status === 401 &&
+          err.error.message === 'Authentication required' &&
+          refreshToken
         ) {
           return this._authService.refreshToken(refreshToken).pipe(
             switchMap((data) => {
@@ -42,6 +89,16 @@ export class RefreshTokenInterceptor implements HttpInterceptor {
             })
           );
         }
+
+        if (
+          err.status === 403 &&
+          err.error.message === 'Token is invalid' &&
+          refreshToken
+        ) {
+          this._authService.logout();
+          return throwError(() => err);
+        }
+
         return throwError(() => err);
       })
     );
